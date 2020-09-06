500630d65a30098cced2bdcbd5e7a7686d3ccb98
Nwaokocha Victor <nvonweb@outlook.com>
2020-09-06 00:05:37 -0400

    add mux server

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
-package gratitude
+package main
 
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"scheduler0/server/src/utils"
+)
+
+func main() {
+	router := mux.NewRouter()
+	port := os.Getenv("PORT")
+
+
+	log.Println("Server is running on port", port)
+	err := http.ListenAndServe(utils.GetPort(), router)
+	if err != nil {
+		panic(err)
+	}
+}
